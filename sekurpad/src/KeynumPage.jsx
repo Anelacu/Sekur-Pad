c52fb6b8d28af06e71c8758755f40756798cb2d7
Ivan Nikitin <nikitin.ivan.dev@gmail.com>
2021-02-11 04:23:42 +0000

    Box modifiers

--- sekurpad/src/KeynumPage.jsx
+++ sekurpad/src/KeynumPage.jsx
@@ -42,168 +42,281 @@ export default function KeynumPage(props) {
     }
 
     return (
-        <Container
-            style={{marginTop: '12%', marginLeft: '30%'}}
-        >
-            <Row
+        <>
+            <Container
                 style={{
-                    marginBottom: '20px'
-                }}>
-                <div
+                    marginLeft: '35%',
+                    marginTop: '3%'
+                }}
+            >
+                <Row>
+                    <Col
+                        xs={1}
+                    >
+                        <p
+                            style={{
+                                fontSize: "60px",
+                            }}
+                        >
+                            ▯
+                        </p>
+                    </Col>
+
+                    <Col
+                        xs={1}
+                    >
+                        <p
+                            style={{
+                                fontSize: "60px",
+                            }}
+                        >
+                            ▯
+                        </p>
+                    </Col>
+
+                    <Col
+                        xs={1}
+                    >
+                        <p
+                            style={{
+                                fontSize: "60px",
+                            }}
+                        >
+                            ▯
+                        </p>
+                    </Col>
+
+                    <Col
+                        xs={1}
+                    >
+                        <p
+                            style={{
+                                fontSize: "60px",
+                            }}
+                        >
+                            ▯
+                        </p>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col
+                        xs={1}
+                    >
+                        <p
+                            style={{
+                                fontSize: "25px",
+                            }}
+                        >
+                            +0
+                        </p>
+                    </Col>
+
+                    <Col
+                        xs={1}
+                    >
+                        <p
+                            style={{
+                                fontSize: "25px",
+                            }}
+                        >
+                            +0
+                        </p>
+                    </Col>
+
+                    <Col
+                        xs={1}
+                    >
+                        <p
+                            style={{
+                                fontSize: "25px",
+                            }}
+                        >
+                            -2
+                        </p>
+                    </Col>
+
+                    <Col
+                        xs={1}
+                    >
+                        <p
+                            style={{
+                                fontSize: "25px",
+                            }}
+                        >
+                            +1
+                        </p>
+                    </Col>
+                </Row>
+
+
+            </Container>
+            <Container
+                style={{
+                    marginTop: '5%',
+                    marginLeft: '30%'
+                }}
+            >
+                <Row
                     style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center"
+                        marginBottom: '20px'
+                    }}>
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center"
+                        }}
+                    >
+                        <h1
+                            style={{
+                                textAlign: "center",
+                            }}
+                        >{'>' + pin}
+                        </h1>
+                    </div>
+                </Row>
+                <Row>
+                    <Col
+                        xs={2}
+                    >
+                        <Button variant="primary" size="lg" block
+                                value={"1"}
+                                onClick={e => handleButton(e)}
+                        >
+                            1
+                        </Button>
+                    </Col>
+                    <Col
+                        xs={2}
+                    >
+                        <Button variant="primary" size="lg" block
+                                value={"2"}
+                                onClick={e => handleButton(e)}
+                        >
+                            2
+                        </Button>
+                    </Col>
+                    <Col
+                        xs={2}
+                    >
+                        <Button variant="primary" size="lg" block
+                                value={"3"}
+                                onClick={e => handleButton(e)}
+                        >
+                            3
+                        </Button>
+                    </Col>
+                </Row>
+                <Row
+                    style={{
+                        marginTop: '5px'
+                    }}>
+                    <Col
+                        xs={2}
+                    >
+                        <Button variant="primary" size="lg" block
+                                value={"4"}
+                                onClick={e => handleButton(e)}
+                        >
+                            4
+                        </Button>
+                    </Col>
+                    <Col
+                        xs={2}
+                    >
+                        <Button variant="primary" size="lg" block
+                                value={"5"}
+                                onClick={e => handleButton(e)}
+                        >
+                            5
+                        </Button>
+                    </Col>
+                    <Col
+                        xs={2}
+                    >
+                        <Button variant="primary" size="lg" block
+                                value={"6"}
+                                onClick={e => handleButton(e)}
+                        >
+                            6
+                        </Button>
+                    </Col>
+                </Row>
+                <Row
+                    style={{
+                        marginTop: '5px'
                     }}
                 >
-                    <h1
-                        style={{
-                            textAlign: "center",
-                        }}
-                    >{pin}
-                    </h1>
-                </div>
-            </Row>
-            <Row>
-                <Col
-                    xs={2}
-                >
-                    <Button variant="primary" size="lg" block
-                            value={"1"}
-                            onClick={e => handleButton(e)}
+                    <Col
+                        xs={2}
                     >
-                        1
-                    </Button>
-                </Col>
-                <Col
-                    xs={2}
-                >
-                    <Button variant="primary" size="lg" block
-                            value={"2"}
-                            onClick={e => handleButton(e)}
+                        <Button variant="primary" size="lg" block
+                                value={"7"}
+                                onClick={e => handleButton(e)}
+                        >
+                            7
+                        </Button>
+                    </Col>
+                    <Col
+                        xs={2}
                     >
-                        2
-                    </Button>
-                </Col>
-                <Col
-                    xs={2}
-                >
-                    <Button variant="primary" size="lg" block
-                            value={"3"}
-                            onClick={e => handleButton(e)}
+                        <Button variant="primary" size="lg" block
+                                value={"8"}
+                                onClick={e => handleButton(e)}
+                        >
+                            8
+                        </Button>
+                    </Col>
+                    <Col
+                        xs={2}
                     >
-                        3
-                    </Button>
-                </Col>
-            </Row>
-            <Row
-                style={{
-                    marginTop: '5px'
-                }}>
-                <Col
-                    xs={2}
+                        <Button variant="primary" size="lg" block
+                                value={"9"}
+                                onClick={e => handleButton(e)}
+                        >
+                            9
+                        </Button>
+                    </Col>
+                </Row>
+                <Row
+                    style={{
+                        marginTop: '5px'
+                    }}
                 >
-                    <Button variant="primary" size="lg" block
-                            value={"4"}
-                            onClick={e => handleButton(e)}
+                    <Col
+                        xs={2}
                     >
-                        4
-                    </Button>
-                </Col>
-                <Col
-                    xs={2}
-                >
-                    <Button variant="primary" size="lg" block
-                            value={"5"}
-                            onClick={e => handleButton(e)}
+                        <Button variant="primary" size="lg" block
+                                value={""}
+                                onClick={e => handleButton(e)}
+                        >
+                            Submit
+                        </Button>
+                    </Col>
+                    <Col
+                        xs={2}
                     >
-                        5
-                    </Button>
-                </Col>
-                <Col
-                    xs={2}
-                >
-                    <Button variant="primary" size="lg" block
-                            value={"6"}
-                            onClick={e => handleButton(e)}
-                    >
-                        6
-                    </Button>
-                </Col>
-            </Row>
-            <Row
-                style={{
-                    marginTop: '5px'
-                }}
-            >
-                <Col
-                    xs={2}
-                >
-                    <Button variant="primary" size="lg" block
-                            value={"7"}
-                            onClick={e => handleButton(e)}
-                    >
-                        7
-                    </Button>
-                </Col>
-                <Col
-                    xs={2}
-                >
-                    <Button variant="primary" size="lg" block
-                            value={"8"}
-                            onClick={e => handleButton(e)}
-                    >
-                        8
-                    </Button>
-                </Col>
-                <Col
-                    xs={2}
-                >
-                    <Button variant="primary" size="lg" block
-                            value={"9"}
-                            onClick={e => handleButton(e)}
-                    >
-                        9
-                    </Button>
-                </Col>
-            </Row>
-            <Row
-                style={{
-                    marginTop: '5px'
-                }}
-            >
-                <Col
-                    xs={2}
-                >
-                    <Button variant="primary" size="lg" block
-                            value={""}
-                            onClick={e => handleButton(e)}
-                    >
-                        Submit
-                    </Button>
-                </Col>
-                <Col
-                    xs={2}
-                >
-                    <Button variant="primary" size="lg" block
-                            value={"0"}
-                            onClick={e => handleButton(e)}
-                    >
-                        0
-                    </Button>
-                </Col>
+                        <Button variant="primary" size="lg" block
+                                value={"0"}
+                                onClick={e => handleButton(e)}
+                        >
+                            0
+                        </Button>
+                    </Col>
 
-                <Col
-                    xs={2}
-                >
-                    <Button variant="primary" size="lg" block
-                            value={"9"}
-                            onClick={handleButtonDel}
+                    <Col
+                        xs={2}
                     >
-                        DEL
-                    </Button>
-                </Col>
-            </Row>
-        </Container>
+                        <Button variant="primary" size="lg" block
+                                value={"9"}
+                                onClick={handleButtonDel}
+                        >
+                            DEL
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     )
 }
