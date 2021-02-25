1a024f55d29b1eea021c2f271acb2efa24414d1c
Anelacu <mmm.lll.2000@gmail.com>
2021-02-25 14:32:33 +0000

    timezone fix

--- sekurpad/src/Logger.jsx
+++ sekurpad/src/Logger.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { nanoid } from 'nanoid'
 import { ApiEndPoints } from "./ApiEndPoints";
 
-const user_uuid = nanoid() + "_" + new Date().toLocaleString();
+const user_uuid = nanoid() + "_" + new Date().toISOString();
 const logActivity = function (activityName) {
     axios.post(ApiEndPoints.createLog, {
         userUuid: user_uuid,
